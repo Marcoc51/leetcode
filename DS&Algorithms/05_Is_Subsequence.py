7e93e5aebba20b9c49b3c830a8bc41661a7cccb5
Marc Atef <64839030+Marcoc51@users.noreply.github.com>
2022-11-29 19:51:18 +0200

    Create 05_Is_Subsequence.py

---
+++ DS&Algorithms/05_Is_Subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        j = 0
+        if len(s) == 0:
+            return True
+        for i in range(len(t)):
+            if t[i] == s[j]:
+                j += 1
+            if j == len(s):
+                return True
+        return False
