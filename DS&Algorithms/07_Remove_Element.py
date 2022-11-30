f91066d0abe68e14507b00693e05757355bfe2f7
Marc Atef <64839030+Marcoc51@users.noreply.github.com>
2022-11-30 08:11:32 +0200

    Create 07_Remove_Element.py

---
+++ DS&Algorithms/07_Remove_Element.py
@@ -0,0 +1,6 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] == val:
+                nums.pop(i)
+        return len(nums)
