9be899e72a5069daf8a38dd9141445022c833cb5
Marc Sanad <64839030+Marcoc51@users.noreply.github.com>
2023-05-19 20:47:32 +0300

    Create 11_Employees_With_Missing_Information.sql

---
+++ SQL I/11_Employees_With_Missing_Information.sql
@@ -0,0 +1,7 @@
+SELECT employee_id
+FROM employees
+WHERE employee_id NOT IN (SELECT employee_id FROM salaries)
+UNION
+SELECT employee_id
+FROM salaries
+WHERE employee_id NOT IN (SELECT employee_id FROM employees);
