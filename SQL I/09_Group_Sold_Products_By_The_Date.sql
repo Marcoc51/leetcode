71b1b60860ab965fce7c09246c5a32abb8bdf1f3
Marc Sanad <64839030+Marcoc51@users.noreply.github.com>
2024-05-22 08:34:30 +0300

    Update 09_Group_Sold_Products_By_The_Date.sql

--- SQL I/09_Group_Sold_Products_By_The_Date.sql
+++ SQL I/09_Group_Sold_Products_By_The_Date.sql
@@ -1,6 +1,15 @@
-SELECT
-sell_date, COUNT(DISTINCT product) AS num_sold, 
-GROUP_CONCAT(DISTINCT product SEPARATOR ',') as products
+-- MySQL Solution
+SELECT sell_date
+      ,COUNT(DISTINCT product) AS num_sold
+      ,GROUP_CONCAT(DISTINCT product SEPARATOR ',') as products
 FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date;
+
+-- SQL Server Solution
+SELECT sell_date
+      ,COUNT(product) AS num_sold
+      ,STRING_AGG(product, ',') WITHIN GROUP (ORDER BY product) AS products
+FROM (SELECT DISTINCT sell_date, product FROM Activities) AS a
 GROUP BY sell_date
 ORDER BY sell_date;
