2c02d88ede85a87a4031f20f7250c17edffaa02d
Marc Sanad <64839030+Marcoc51@users.noreply.github.com>
2025-05-04 06:55:32 +0300

    Create 65_Odd_and_Even_Transactions.sql

---
+++ SQL I/65_Odd_and_Even_Transactions.sql
@@ -0,0 +1,10 @@
+SELECT
+    transaction_date
+    ,SUM(IIF(amount % 2 = 1, amount, 0)) AS odd_sum
+    ,SUM(IIF(amount % 2 = 0, amount, 0)) AS even_sum
+FROM
+    transactions
+GROUP BY
+    transaction_date
+ORDER BY
+    transaction_date;
